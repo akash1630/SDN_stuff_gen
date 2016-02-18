e3ef04338a4264ce8a6e4c6c0f522cce3a530abf
Akash Verma <akash@Akashs-MacBook-Pro.local>
2016-02-18 16:46:49 -0500

    bug fixes

--- watermark.py
+++ watermark.py
@@ -103,7 +103,7 @@ def _handle_PacketIn (event):
   	mac_port_dict[packet.src] = event.port
   if (packet.dst not in mac_port_dict or skip_add_to_dict == 1):
     log.debug("flooding to all ports as no entry in dictionary and skip_add_to_dict is %i", skip_add_to_dict)
-	  flood_packet(event, of.OFPP_ALL)
+    flood_packet(event, of.OFPP_ALL)
   else:
 	 port = mac_port_dict[packet.dst]
 	 log.debug("setting a flow table entry as matching entry found in dict - " + str(packet.src) + "    " + str(packet.dst))
