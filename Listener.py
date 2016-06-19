cb2b042c0a4b4da35865782b9ffc4a4dd6d7f765
mininet <akash1630@gmail.com>
2016-06-19 15:36:31 -0700

    changes

--- Listener.py
+++ Listener.py
@@ -9,39 +9,41 @@ class MessageHandler(SocketServer.StreamRequestHandler):
     def handle(self):
     	try:
     		log.debug("----- handling message ------")
-			self.data = self.rfile.readline().strip()
-			log.debug("received message : " + self.data)
+		self.data = self.request.recv(1024).strip()
+		log.debug("received message : " + self.data)
 	        host_msg = self.data.split(',')
 	        if ('taint' in host_msg[0].lower()):
-		        rhost = ipaddr.IPAddress(host_msg[1])
-		        rport = host_msg[2]
+		        #rhost = ipaddr.IPAddress(host_msg[1])
+			tainted_host = host_msg[1]
+		        tainted_port = host_msg[2]
 
 			log.debug("[+] Rcvd Tainted Conn: "+str(self.data))
 
-			if ((rhost) and (int(rport) > 0) and (int(rport) < 65535)):
-				rtn_msg = 'ack,'+str(rhost)+','+str(rport)+","+str(lport)+'\n'
+			if ((tainted_host) and (int(tainted_port) > 0) and (int(tainted_port) < 65535)):
+				rtn_msg = 'ack,'+str(tainted_host)+','+str(tainted_port)+","'\n'
 			        self.wfile.write(rtn_msg)
 			        self.wfile.close()
-		cur_thread = threading.current_thread()
-        response = "{}: {}".format(cur_thread.name, self.data)
-        self.request.sendall(response)
 	except Exception as e:
 		log.error('[!] Failed Handler: '+str(e))
 
 class ListenThread(threading.Thread):
 	def __init__(self,host,port):
 		try:
-			Thread.__init__(self)
+			threading.Thread.__init__(self)
 	        	self.host='0.0.0.0'
 	        	self.port=port
 	        	self.server = SocketServer.TCPServer((self.host,self.port), MessageHandler)
+			log.debug('------runing thread------')
+			self.server.allow_reuse_address = True
+			self.server.serve_forever()
 			log.debug('[+] Listener Initialized.')
 		except Exception as e:
 			log.error('[!] Failed to Initialize: '+str(e))
 
     	def run(self):
 		try:
-			self.server.allow_reuse_address = True 
+			self.server.allow_reuse_address = True
+			log.debug('----running thread-----') 
 			#self.server.server_bind()     
 			#self.server.server_activate() 
 	        	self.server.serve_forever()
