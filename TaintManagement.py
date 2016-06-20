8d0fb4ffa9925abc734f52fec908b97e9068ca7e
Akash Verma <akash@Akashs-MacBook-Pro.local>
2016-06-20 11:01:31 -0400

    ctrl changes

--- TaintManagement.py
+++ TaintManagement.py
@@ -13,7 +13,7 @@ from threading import Thread
 import numpy as np
 import scipy as sp
 
-from Listener import ListenThread
+#from Listener import ListenThread
 
 from pox.openflow.of_json import *
 #import taint_db_impl
@@ -380,3 +380,58 @@ def launch ():
   #thr = Thread(target = taint_msg_listener, name = 'listen_for_messages')
   #thr.start()
 
+class MessageHandler(SocketServer.StreamRequestHandler):
+    def handle(self):
+      try:
+        log.debug("----- handling message ------")
+    self.data = self.request.recv(1024).strip()
+    log.debug("received message : " + self.data)
+          host_msg = self.data.split(',')
+          if ('taint' in host_msg[0].lower()):
+            #rhost = ipaddr.IPAddress(host_msg[1])
+      host_to_taint = host_msg[1]
+            tainted_dest_port = host_msg[2]
+            tainted_src_port = host_msg[3]
+
+      log.debug("[+] Rcvd Tainted Conn: "+str(self.data))
+
+      if ((tainted_host) and (int(tainted_dest_port) > 0) and (int(tainted_dest_port) < 65535)):
+        if((int(tainted_src_port) > 0) and (int(tainted_src_port) < 65535)):
+          rtn_msg = 'ack,'+str(host_to_taint)+','+str(tainted_dest_port)+","+str(tainted_src_port)+'\n'
+                self.wfile.write(rtn_msg)
+                self.wfile.close()
+                taint_action()
+
+  except Exception as e:
+    log.error('[!] Failed Handler: '+str(e))
+
+class ListenThread(threading.Thread):
+  def __init__(self,host,port):
+    try:
+      threading.Thread.__init__(self)
+            self.host='0.0.0.0'
+            self.port=port
+            self.server = SocketServer.TCPServer((self.host,self.port), MessageHandler)
+      log.debug('------runing thread------')
+      self.server.allow_reuse_address = True
+      self.server.serve_forever()
+      log.debug('[+] Listener Initialized.')
+    except Exception as e:
+      log.error('[!] Failed to Initialize: '+str(e))
+
+      def run(self):
+    try:
+      self.server.allow_reuse_address = True
+      log.debug('----running thread-----') 
+      #self.server.server_bind()     
+      #self.server.server_activate() 
+            self.server.serve_forever()
+    except Exception as e:
+      log.error('[!] Failed Run: '+str(e))
+
+  def end(self):
+    try:
+      log.debug(RED+'[!] Shutting Down SocketServer')
+      self.server.shutdown()
+    except Exception as e:
+      log.error('[!] Failed to Shutdown() Server: '+str(e))
