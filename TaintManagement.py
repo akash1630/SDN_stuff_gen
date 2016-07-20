73d4f0ef19c4976cc04c76d0f2d0f81ae53dc2e7
Akash Verma <akash1630@gmail.com>
2016-07-20 09:20:23 -0400

    changes

--- TaintManagement.py
+++ TaintManagement.py
@@ -36,22 +36,22 @@ samples = np.random.normal(250, 35, 1000)
 ############################################################################
 
 temp_map = {}
-temp_map['169.254.7.2']='192.168.56.101'
-temp_map['169.254.4.222']='192.168.56.102'
-temp_map['169.254.5.1'] = '192.168.56.104'
+temp_map['10.4.4.2']='192.168.158.192'
+temp_map['10.4.4.3']='192.168.158.193'
+temp_map['10.4.4.4'] ='192.168.158.194'
 
 #############################################################################
 #define internal network here - ****IMPORTANT****
 #############################################################################
-internal_ips = "169.254.0.0/16"
+internal_ips = "10.4.4.0/24"
 internal_network = ipaddr.IPNetwork(internal_ips)
-restrict_if_pivot_ips = "169.254.4.0/24"
+restrict_if_pivot_ips = "10.4.4.0/24"
 restrict_if_pivot_network = ipaddr.IPNetwork(restrict_if_pivot_ips)
-throttle_outbound_if_pivot_ips = "169.254.7.0/24"
+throttle_outbound_if_pivot_ips = "10.4.4.0/24"
 throttle_outbound_if_pivot_network = ipaddr.IPNetwork(throttle_outbound_if_pivot_ips)
-hosts_without_agent = "10.0.0.5/32"
+hosts_without_agent = "10.4.4.5/32"
 network_hosts_without_agent = ipaddr.IPNetwork(hosts_without_agent)
-protected_resources = ["169.254.7.2"]       #list of protected resources
+protected_resources = ["10.4.4.4"]       #list of protected resources
 
 
 #############################################################################
@@ -289,6 +289,7 @@ def _handle_PacketIn (event):
   skip_add_to_dict_dest = 0
   skip_add_to_dict_src = 0
   is_tcp_ack = 0
+  is_icmp_pack = 0
   srcport = 0
   dstport = 0
 
@@ -323,6 +324,10 @@ def _handle_PacketIn (event):
       #mac_port_dict[packet.src] = event.port
       is_tcp_ack = 1
 
+  icmp = packet.find("icmp")
+  if icmp:
+  	is_icmp_pack = 1
+
   if srcip in suspected_hosts:
     delete_flow_entries(srcip)
     drop_packet(event)
@@ -332,7 +337,7 @@ def _handle_PacketIn (event):
   #log.debug("packet forwarding  " + src_eth_addr + "  " + dest_eth_addr)
   is_tcp_ack = 0
   
-  if is_tcp_ack == 0:
+  if (is_tcp_ack == 0 and is_icmp_pack ==0):
     if (srcip in protected_resources):
       if(dstip in protected_resources):
         log.debug("protected to protected communication")
@@ -381,6 +386,9 @@ def taint_action(ip, port):
   if ip in protected_resources:
     log.debug(" ------ Host to be tainted is a protected resource. No Action. Returning. --------------")
     return
+  if(port < 1 or port > 65534):
+  	log.debug("----- Either ICMP packet or port not valid. No Action. Returning. ------------ ")
+  	return
   log.debug("<<<<<<<  Performing taint actions ip : "+ip + "  port :"+ str(port) +" >>>>>>>>>>")
   if(network_hosts_without_agent.Contains(ipaddr.IPAddress(ip))):
     log.debug("### Host does not have an agent running - taint the whole host   ###")
